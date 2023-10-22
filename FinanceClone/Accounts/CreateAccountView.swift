d79fc10e308a26b513214ada98737c1ecc1ebd2a
Gan Tu <github@tugan.dev>
2023-10-22 19:10:48 -0400

    feat: add create account

---
+++ FinanceClone/Accounts/CreateAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAccountView.swift
+//  FinanceClone
+//
+//  Created by Gan Tu on 10/22/23.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAccountView()
+}
